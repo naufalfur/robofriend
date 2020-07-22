ce9b22df5667d41d89e5344cdda522809413c18d
naufalfur <muhnaufalfurqon77@gmail.com>
2020-07-22 12:02:04 +0700

    Updates

---
+++ precache-manifest.6c5e726e3c3e20b25279ecea51e9b0eb.js
@@ -0,0 +1,45 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a1d93b3f599b68cd38b133f6177708",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "8d64515b55ef7c162725",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7d88673d608f2cbfba05",
+    "url": "/robofriend/static/css/main.c32cd3a2.chunk.css"
+  },
+  {
+    "revision": "8d64515b55ef7c162725",
+    "url": "/robofriend/static/js/2.b436b838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.b436b838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d88673d608f2cbfba05",
+    "url": "/robofriend/static/js/main.a97cb83e.chunk.js"
+  },
+  {
+    "revision": "1b4162bcd83e4429ae73",
+    "url": "/robofriend/static/js/runtime-main.8eb00657.js"
+  },
+  {
+    "url": "/robofriend/static/media/b593a614952261da06c53a02d1954f44.2b35f2bc.eot"
+  },
+  {
+    "url": "/robofriend/static/media/b593a614952261da06c53a02d1954f44.443e71e6.svg"
+  },
+  {
+    "url": "/robofriend/static/media/b593a614952261da06c53a02d1954f44.b593a614.ttf"
+  },
+  {
+    "url": "/robofriend/static/media/b593a614952261da06c53a02d1954f44.b6e9bb37.woff"
+  },
+  {
+    "url": "/robofriend/static/media/b593a614952261da06c53a02d1954f44.e346874e.woff2"
+  }
+]);
